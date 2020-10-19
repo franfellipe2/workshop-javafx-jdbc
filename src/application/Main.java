754df0182fab7050fb59c22657db1543c5bf65b6
Francisco S. Filho <franfellipe2@hotmail.com>
2020-10-19 18:19:18 -0300

    Main view design created

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,25 +6,22 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-
-		/*
-		 * try { BorderPane root = new BorderPane(); Scene scene = new
-		 * Scene(root,400,400);
-		 * scene.getStylesheets().add(getClass().getResource("application.css").
-		 * toExternalForm()); primaryStage.setScene(scene); primaryStage.show(); }
-		 * catch(Exception e) { e.printStackTrace(); }
-		 */
-
+	
 		try {
 
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent root = loader.load();
-			Scene scene = new Scene(root);
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene scene = new Scene(scrollPane);
 			primaryStage.setTitle("Sample Aplication: javaFX with JDBC");
 			primaryStage.setScene(scene);
 			primaryStage.show();
